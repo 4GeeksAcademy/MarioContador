7683a4cc49670f54dea18359a3e052339cb8cfb3
marioteli96 <cacaopepino@gmail.com>
2024-09-17 16:10:33 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 
-// Componente principal
 const Contador = () => {
   const [time, setTime] = useState(0);
 
-  // useEffect para actualizar el contador cada segundo
+ 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(prevTime => prevTime + 1);
-    }, 1000); // Actualizar cada 1 segundo
+    }, 1000); // 
 
-    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+    return () => clearInterval(interval);
   }, []);
 
-  // Función para obtener los dígitos individuales del tiempo
+ 
   const formatTime = (num) => {
     return num.toString().padStart(6, '0').split('');
   };
 
   return (
     <div className="contador-container">
-      {/* Renderizamos los dígitos */}
       {formatTime(time).map((digit, index) => (
         <div key={index} className="digit-box">
           {digit}
